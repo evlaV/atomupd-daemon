74500128ad4b24faf992f3c5f69d7a0b4bdb8c19
Ludovico de Nittis <ludovico.denittis@collabora.com>
2022-01-25 12:24:58 +0100

    au-atomupd1-impl: Add a stub for all the provided methods
    Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

--- atomupd-daemon/au-atomupd1-impl.c
+++ atomupd-daemon/au-atomupd1-impl.c
@@ -30,6 +30,7 @@
 #include <glib-unix.h>
 #include <gio/gio.h>
 
+#include "utils.h"
 #include "au-atomupd1-impl.h"
 
 struct _AuAtomupd1Impl
@@ -37,9 +38,109 @@ struct _AuAtomupd1Impl
   AuAtomupd1Skeleton parent_instance;
 };
 
+static gboolean
+au_atomupd1_impl_handle_check_for_updates (AuAtomupd1 *object,
+                                           GDBusMethodInvocation *invocation,
+                                           GVariant *arg_options)
+{
+  g_warning ("TODO: check for update is just a stub!");
+
+  au_atomupd1_complete_check_for_updates (object,
+                                          g_steal_pointer (&invocation),
+                                          g_variant_new ("a{sa{sv}}", NULL),
+                                          g_variant_new ("a{sa{sv}}", NULL));
+
+  return TRUE;
+}
+
+static gboolean
+au_atomupd1_impl_handle_cancel_update (AuAtomupd1 *object,
+                                       GDBusMethodInvocation *invocation)
+{
+  g_warning ("TODO: cancel update is just a stub!");
+
+  if (au_atomupd1_get_update_status (object) != AU_UPDATE_STATUS_IN_PROGRESS)
+    {
+      g_dbus_method_invocation_return_error (g_steal_pointer (&invocation),
+                                             G_DBUS_ERROR,
+                                             G_DBUS_ERROR_FAILED,
+                                             "There isn't an update in progress that can be cancelled");
+      return TRUE;
+    }
+
+  // au_atomupd1_set_update_status (object, AU_UPDATE_STATUS_CANCELLED);
+
+  au_atomupd1_complete_cancel_update (object, g_steal_pointer (&invocation));
+
+  return TRUE;
+}
+
+static gboolean
+au_atomupd1_impl_handle_pause_update (AuAtomupd1 *object,
+                                      GDBusMethodInvocation *invocation)
+{
+  g_warning ("TODO: pause update is just a stub!");
+
+  if (au_atomupd1_get_update_status (object) != AU_UPDATE_STATUS_IN_PROGRESS)
+    {
+      g_dbus_method_invocation_return_error (g_steal_pointer (&invocation),
+                                             G_DBUS_ERROR,
+                                             G_DBUS_ERROR_FAILED,
+                                             "There isn't an update in progress that can be paused");
+      return TRUE;
+    }
+
+  // au_atomupd1_set_update_status (object, AU_UPDATE_STATUS_PAUSED);
+
+  au_atomupd1_complete_pause_update (object, g_steal_pointer (&invocation));
+
+  return TRUE;
+}
+
+static gboolean
+au_atomupd1_impl_handle_start_update (AuAtomupd1 *object,
+                                      GDBusMethodInvocation *invocation,
+                                      const gchar *arg_id)
+{
+  g_warning ("TODO: start update is just a stub!");
+
+  // au_atomupd1_set_update_status (object, AU_UPDATE_STATUS_IN_PROGRESS);
+
+  au_atomupd1_complete_start_update (object, g_steal_pointer (&invocation));
+
+  return TRUE;
+}
+
+static gboolean
+au_atomupd1_impl_handle_resume_update (AuAtomupd1 *object,
+                                       GDBusMethodInvocation *invocation)
+{
+  g_warning ("TODO: resume update is just a stub!");
+
+  if (au_atomupd1_get_update_status (object) != AU_UPDATE_STATUS_PAUSED)
+    {
+      g_dbus_method_invocation_return_error (g_steal_pointer (&invocation),
+                                             G_DBUS_ERROR,
+                                             G_DBUS_ERROR_FAILED,
+                                             "There isn't a paused update that can be resumed");
+      return TRUE;
+    }
+
+  // au_atomupd1_set_update_status (object, AU_UPDATE_STATUS_IN_PROGRESS);
+
+  au_atomupd1_complete_resume_update (object, g_steal_pointer (&invocation));
+
+  return TRUE;
+}
+
 static void
 init_atomupd1_iface (AuAtomupd1Iface *iface)
 {
+  iface->handle_cancel_update = au_atomupd1_impl_handle_cancel_update;
+  iface->handle_check_for_updates = au_atomupd1_impl_handle_check_for_updates;
+  iface->handle_pause_update = au_atomupd1_impl_handle_pause_update;
+  iface->handle_resume_update = au_atomupd1_impl_handle_resume_update;
+  iface->handle_start_update = au_atomupd1_impl_handle_start_update;
 }
 
 G_DEFINE_TYPE_WITH_CODE (AuAtomupd1Impl, au_atomupd1_impl, AU_TYPE_ATOMUPD1_SKELETON,
@@ -68,6 +169,9 @@ AuAtomupd1 *
 au_atomupd1_impl_new (void)
 {
   AuAtomupd1 *atomupd = g_object_new (AU_TYPE_ATOMUPD1_IMPL, NULL);
+
+  /* We currently only have the version 1 of this interface */
+  au_atomupd1_set_version (atomupd, 1);
 
   return atomupd;
 }
