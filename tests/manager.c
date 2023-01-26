60d5499bc082988623d364b38fafdad354320b88
Ludovico de Nittis <ludovico.denittis@collabora.com>
2023-01-26 15:12:08 +0100

    tests: Add coverage for atomupd-manager
    Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

---
+++ tests/manager.c
@@ -0,0 +1,263 @@
+/*
+ * Copyright © 2023 Collabora Ltd.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <errno.h>
+#include <libelf.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <gio/gio.h>
+#include <glib-unix.h>
+#include <glib.h>
+#include <glib/gstdio.h>
+
+#include "atomupd-daemon/utils.h"
+#include "fixture.h"
+#include "services.h"
+
+#define _skip_if_daemon_is_running(_bus, _error)                                         \
+   if (au_tests_is_daemon_service_running(bus, _error)) {                                \
+      g_test_skip("Can't run this test if another instance of the Atomupd "              \
+                  "daemon service is already running");                                  \
+      return;                                                                            \
+   }
+
+static gchar *
+_au_execute_manager(const gchar *command,
+                    const gchar *argument,
+                    gchar **envp,
+                    GError **error)
+{
+   g_autofree gchar *output = NULL;
+   gint wait_status = 0;
+
+   const gchar *systemctl_argv[] = {
+      "atomupd-manager", "--session", command, argument, NULL,
+   };
+
+   g_return_val_if_fail(error == NULL || *error == NULL, NULL);
+
+   if (!g_spawn_sync(NULL, /* working directory */
+                     (gchar **)systemctl_argv, envp, G_SPAWN_SEARCH_PATH,
+                     NULL,          /* child setup */
+                     NULL,          /* user data */
+                     &output, NULL, /* stderr */
+                     &wait_status, error)) {
+      return NULL;
+   }
+
+   g_print("atomupd-manager output: %s\n", output);
+
+   if (!g_spawn_check_wait_status(wait_status, error))
+      return NULL;
+
+   return g_steal_pointer(&output);
+}
+
+typedef struct {
+   const gchar *update_json;
+   const gchar *output_contains[10];
+   const gchar *output_does_not_contain[10];
+} CheckTest;
+
+static const CheckTest check_test[] =
+{
+  {
+    .update_json = "update_empty.json",
+    .output_contains =
+    {
+      "No update available",
+    },
+    .output_does_not_contain =
+    {
+      "Updates available:",
+      "Updates available later:",
+    },
+  },
+
+  {
+    .update_json = "update_one_minor.json",
+    .output_contains =
+    {
+      "Updates available:",
+      "20220227.3",
+    },
+    .output_does_not_contain =
+    {
+      "Updates available later:",
+      "No update available",
+    },
+  },
+
+  {
+    .update_json = "update_one_minor_one_major.json",
+    .output_contains =
+    {
+      "Updates available:",
+      "20220120.1",
+      "20220202.1",
+    },
+    .output_does_not_contain =
+    {
+      "Updates available later:",
+      "No update available",
+    },
+  },
+
+  {
+    .update_json = "update_three_minors.json",
+    .output_contains =
+    {
+      "Updates available:",
+      "20211225.1",
+      "steamdeck",
+      "40310422",
+      "Updates available later:",
+      "20220101.1",
+      "20220227.3",
+    },
+    .output_does_not_contain =
+    {
+      "No update available",
+    },
+  },
+};
+
+static void
+test_check_updates(Fixture *f, gconstpointer context)
+{
+   g_autoptr(GDBusConnection) bus = NULL;
+   g_autoptr(GError) error = NULL;
+   gsize i, j;
+
+   bus = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, NULL);
+
+   _skip_if_daemon_is_running(bus, NULL);
+
+   for (i = 0; i < G_N_ELEMENTS(check_test); i++) {
+      g_autoptr(GSubprocess) daemon_proc = NULL;
+      g_autofree gchar *update_file_path = NULL;
+      g_autofree gchar *output = NULL;
+      const CheckTest ct = check_test[i];
+
+      update_file_path = g_build_filename(f->srcdir, "data", ct.update_json, NULL);
+      f->test_envp =
+         g_environ_setenv(f->test_envp, "G_TEST_UPDATE_JSON", update_file_path, TRUE);
+
+      daemon_proc =
+         au_tests_start_daemon_service(bus, f->manifest_path, f->conf_path, f->test_envp);
+
+      output = _au_execute_manager("check", NULL, f->test_envp, &error);
+      g_assert_no_error(error);
+
+      g_debug("%s", output);
+
+      for (j = 0; ct.output_contains[j] != NULL; j++)
+         g_assert_nonnull(strstr(output, ct.output_contains[j]));
+
+      for (j = 0; ct.output_does_not_contain[j] != NULL; j++)
+         g_assert_null(strstr(output, ct.output_does_not_contain[j]));
+
+      au_tests_stop_daemon_service(daemon_proc);
+   }
+}
+
+static void
+test_multiple_method_calls(Fixture *f, gconstpointer context)
+{
+   g_autoptr(GSubprocess) daemon_proc = NULL;
+   g_autoptr(GDBusConnection) bus = NULL;
+   g_autoptr(GError) error = NULL;
+   g_autofree gchar *update_file_path = NULL;
+   g_autofree gchar *output = NULL;
+   g_autofree gchar *steamos_branch = NULL;
+   g_autofree gchar *parsed_variant = NULL;
+   int fd;
+
+   bus = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, NULL);
+
+   _skip_if_daemon_is_running(bus, NULL);
+
+   update_file_path = g_build_filename(f->srcdir, "data", "update_one_minor.json", NULL);
+   f->test_envp =
+      g_environ_setenv(f->test_envp, "G_TEST_UPDATE_JSON", update_file_path, TRUE);
+
+   fd = g_file_open_tmp("steamos-branch-XXXXXX", &steamos_branch, &error);
+   g_assert_no_error(error);
+   close(fd);
+   f->test_envp =
+      g_environ_setenv(f->test_envp, "AU_CHOSEN_BRANCH_FILE", steamos_branch, TRUE);
+
+   daemon_proc =
+      au_tests_start_daemon_service(bus, f->manifest_path, f->conf_path, f->test_envp);
+
+   g_assert_no_error(error);
+   output = _au_execute_manager("switch-variant", "steamdeck-beta", f->test_envp, &error);
+   g_assert_no_error(error);
+   g_file_get_contents(steamos_branch, &parsed_variant, NULL, &error);
+   g_assert_no_error(error);
+   g_assert_cmpstr(parsed_variant, ==, "beta");
+
+   g_clear_pointer(&output, g_free);
+   g_clear_pointer(&parsed_variant, g_free);
+
+   output = _au_execute_manager("switch-variant", "steamdeck", f->test_envp, &error);
+   g_assert_no_error(error);
+   g_file_get_contents(steamos_branch, &parsed_variant, NULL, &error);
+   g_assert_no_error(error);
+   g_assert_cmpstr(parsed_variant, ==, "rel");
+
+   g_clear_pointer(&output, g_free);
+
+   output = _au_execute_manager("check", NULL, f->test_envp, &error);
+   g_assert_no_error(error);
+   g_assert_nonnull(strstr(output, "20220227.3"));
+
+   g_clear_pointer(&output, g_free);
+
+   g_debug("Starting an update that is expected to complete in 1.5 seconds");
+   output = _au_execute_manager("update", "mock-success", f->test_envp, &error);
+   g_assert_no_error(error);
+   g_assert_nonnull(strstr(output, "Update completed"));
+
+   au_tests_stop_daemon_service(daemon_proc);
+}
+
+int
+main(int argc, char **argv)
+{
+   int ret;
+
+   g_test_init(&argc, &argv, NULL);
+
+#define test_add(_name, _test)                                                           \
+   g_test_add(_name, Fixture, argv[0], au_tests_setup, _test, au_tests_teardown)
+
+   test_add("/manager/check_updates", test_check_updates);
+   test_add("/manager/multiple_method_calls", test_multiple_method_calls);
+
+   ret = g_test_run();
+   return ret;
+}
