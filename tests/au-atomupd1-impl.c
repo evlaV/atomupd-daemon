b2d6e70587ec1b384dc6a1ef8b7289d06a1abc92
Ludovico de Nittis <ludovico.denittis@collabora.com>
2023-09-04 13:47:41 +0200

    Validate buildid of the current system
    When starting up the atomupd daemon, we validate the current system
    buildid. The check is conceptually the same of to what it's done in the
    BuildId class of `steamos-atomupd.git`.
    
    If the buildid is unexpected, this is a clear indication that something
    is wrong with the manifest or the whole system.
    In that case we exit early with an error.
    
    Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

--- tests/au-atomupd1-impl.c
+++ tests/au-atomupd1-impl.c
@@ -135,6 +135,80 @@ test_config_auth(Fixture *f, gconstpointer context)
    }
 }
 
+typedef struct {
+   const gchar *buildid;
+   const gint64 date;
+   const gint64 increment;
+   gboolean valid;
+} BuildidCheckTest;
+
+static const BuildidCheckTest buildid_check_tests[] = {
+   {
+      .buildid = "20230831.1",
+      .date = 20230831,
+      .increment = 1,
+      .valid = TRUE,
+   },
+
+   {
+      .buildid = "23001231.1000",
+      .date = 23001231,
+      .increment = 1000,
+      .valid = TRUE,
+   },
+
+   {
+      .buildid = "19700101",
+      .date = 19700101,
+      .valid = TRUE,
+   },
+
+   { .buildid = "20230832.10" },
+
+   { .buildid = "20231331.1" },
+
+   { .buildid = NULL },
+
+   { .buildid = "" },
+
+   { .buildid = " " },
+
+   { .buildid = "20230831.1b" },
+
+   { .buildid = "2023083a.1" },
+
+   { .buildid = "202308311" },
+
+   { .buildid = "20230831.-1" },
+
+   { .buildid = "20230831.1.2" },
+
+   { .buildid = "2023.100" },
+};
+
+static void
+test_buildid_check(Fixture *f, gconstpointer context)
+{
+   for (gsize i = 0; i < G_N_ELEMENTS(buildid_check_tests); i++) {
+      const BuildidCheckTest *test = &buildid_check_tests[i];
+      g_autoptr(GError) error = NULL;
+      gint64 date = 0;
+      gint64 inc = 0;
+      gboolean result;
+
+      result = _is_buildid_valid(test->buildid, &date, &inc, &error);
+      g_assert_true(result == test->valid);
+
+      if (test->valid)
+         g_assert_no_error(error);
+      else
+         g_assert_error(error, G_IO_ERROR, G_IO_ERROR_FAILED);
+
+      g_assert_cmpint(date, ==, test->date);
+      g_assert_cmpint(inc, ==, test->increment);
+   }
+}
+
 int
 main(int argc, char **argv)
 {
@@ -143,6 +217,7 @@ main(int argc, char **argv)
 #define test_add(_name, _test) g_test_add(_name, Fixture, argv[0], setup, _test, teardown)
 
    test_add("/atomupd1/config_auth", test_config_auth);
+   test_add("/atomupd1/buildid_check", test_buildid_check);
 
    return g_test_run();
 }
